7ee0d0cc12fae7c8c003ae7eee78de2f6b63751f
cheal3 <cheal3@naver.com>
2020-09-03 15:23:30 +0900

    9

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -16,64 +18,64 @@ const styles = theme => ({
     },
     table: {
       minWidth: 1080
+    },
+    progress:{
+      margin: theme.spacing.unit *2
     }
 });
 
-
-
-
-const customers = [
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/any',
-  'name': '염소',
-  'birthday':'031223',
-  'gender':'여자',
-  'job':'대학생'
-},
-{
-'id': 2,
-'image': 'https://placeimg.com/64/64/2',
-'name': '최동욱',
-'birthday': '951211',
-'gender': '남자',
-'job': '인턴'
-},
-{
-'id': 3,
-'image': 'https://placeimg.com/64/64/3',
-'name': '털보',
-'birthday': '961127',
-'gender': '남자',
-'job': '개'
-}
-]
-
-
 class App extends Component{
+
+  state = {
+    customers: '',
+    completed: 0
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(this.progress,20);
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
+  }
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+  callApi = async() =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const {completed } = this.state;
+    this.setState({ completed:completed >=100?0 : completed +1});
+  };
   render(){
-  return (
-    <div>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell> 번호 </TableCell>
-          <TableCell> 이미지 </TableCell>
-          <TableCell> 이름 </TableCell>
-          <TableCell> 생년월일 </TableCell>
-          <TableCell> 성별 </TableCell>
-          <TableCell> 직업 </TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-      {customers.map(c => {
-return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-} )  }
-  </TableBody>
-  </Table>
-</div>
+    const { classes } = this.props;
+    return (
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell> 번호 </TableCell>
+              <TableCell> 이미지 </TableCell>
+              <TableCell> 이름 </TableCell>
+              <TableCell> 생년월일 </TableCell>
+              <TableCell> 성별 </TableCell>
+              <TableCell> 직업 </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {this.state.customers ? this.state.customers.map(c => {
+            return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            }) : ''}
+
+          </TableBody>
+      </Table>
+    </Paper>
     );
   }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -5,8 +5,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello/', (req,res) =>{
-  res.send({ message: "Hello Express!"});
+app.get('/api/customers/', (req,res) =>{
+  res.send([{
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name': '염소',
+    'birthday':'031223',
+    'gender':'여자',
+    'job':'대학생'
+  },
+  {
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '최동욱',
+  'birthday': '951211',
+  'gender': '남자',
+  'job': '인턴'
+  },
+  {
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '털보',
+  'birthday': '961127',
+  'gender': '남자',
+  'job': '개'
+  }
+]);
 
 });
 
