57b010e9587d4e386681c8c14c67ac455a301d95
cheal3 <cheal3@naver.com>
2020-09-04 11:01:14 +0900

    10

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,33 +6,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/customers/', (req,res) =>{
-  res.send([{
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/any',
-    'name': '염소',
-    'birthday':'031223',
-    'gender':'여자',
-    'job':'대학생'
-  },
-  {
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '최동욱',
-  'birthday': '951211',
-  'gender': '남자',
-  'job': '인턴'
-  },
-  {
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '털보',
-  'birthday': '961127',
-  'gender': '남자',
-  'job': '개'
-  }
-]);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host : conf.host,
+  user : conf.user,
+  password: conf.password,
+  port : conf.port,
+  database: conf.database
+});
+connection.connect();
+app.get('/api/customers/', (req,res) =>{
+  connection.query(
+    'SELECT * FROM CUSTOMER',
+    (err , rows, fields) => {
+      res.send(rows);
+    }
+  )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
